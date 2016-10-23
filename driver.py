7469d084c299fb8a9fd46f7a79cff5298e3c80ed
dpickering95 <dmp6026@uncw.edu>
2016-10-23 15:13:41 -0400

    ability to generate starting song pool

--- driver.py
+++ driver.py
@@ -7,7 +7,8 @@ from CSC380 import Song2
 test = Song2.Song2('spotify:track:31n9wiTDFnCE3WJNcSRqL9')
 ## creates a song, which is really just a dictionary of features
 
-print(test.getFeature('liveness'))
-print(test.getArtist())
-print(test.getRelatedArtist())
+#print(test.getFeature('liveness'))
+#print(test.getArtist())
+#print(test.getRelatedArtist())
+print(test.getTopTracks())
 ## print whichever feature we want
