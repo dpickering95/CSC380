225ec7ae9a61b632e2aadc3c0757c4c97b293644
dpickering95 <dmp6026@uncw.edu>
2016-11-03 12:00:17 -0400

    did some stuff

--- driver.py
+++ driver.py
@@ -11,7 +11,8 @@ test = Song2.Song2('spotify:track:31n9wiTDFnCE3WJNcSRqL9')
 #print(test.getArtist())
 #print(test.getRelatedArtist())
 #print(test.getTopTracks())
-topTracks = test.getTopTracks()
-print(test.compare(topTracks))
+songPool = test.getTopTracks()
+nextSong = test.next_song(songPool)
+#print(test.compare(topTracks))
 
 
