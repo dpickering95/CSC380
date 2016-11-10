234ccceb960f7e3ecc6ff5c416e1e2812d330aa2
Matt <mh2296@uncw.edu>
2016-11-10 11:32:53 -0500

    tester for pl1

---
+++ pl1-test.py
@@ -0,0 +1,11 @@
+import pl1
+
+playlist1 = pl1.pl1()
+
+#print(str(playlist1.song_pool))
+playlist1.next_track('finish')
+#print(str(playlist1.song_pool))
+playlist1.next_track('finish')
+#print(str(playlist1.song_pool))
+playlist1.next_track('finish')
+#print(str(playlist1.song_pool))
